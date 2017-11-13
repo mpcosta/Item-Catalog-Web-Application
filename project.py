c22e6be0a011338ac9915f8036d3c3db717d70df
Marco Costa <markkcosta@gmail.com>
2017-11-13 11:21:14 +0000

    Added routing for all pages using flask

---
+++ project.py
@@ -0,0 +1,63 @@
+from flask import Flask, render_template, request, redirect, jsonify, url_for, flash
+from sqlalchemy import create_engine, asc
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Categories, Item, User
+from flask import session as login_session
+import random
+import string
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+import httplib2
+import json
+from flask import make_response
+import requests
+
+app = Flask(__name__)
+
+
+# Connect to Database and create database session
+engine = create_engine('sqlite:///catalog.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
+# Show the Main Page
+@app.route('/')
+@app.route('/catalog/')
+def showCatalog():
+    return "This is catalog main page !"
+
+
+# Show the items related to Category
+@app.route('/catalog/<catalog_name>/items/')
+def showCategoryItems(catalog_name):
+    assert catalog_name == request.view_args['catalog_name']
+    return "This is catalog items !"
+
+# Show specific item info
+@app.route('/catalog/<catalog_name>/<item_name>/')
+def showCategoryItem():
+    return "This is catalog specific item !"
+
+
+# Allow Edit of Item
+@app.route('/catalog/<catalog_name>/<item_name>/edit/', methods=['GET', 'POST'])
+def editCategoryItem():
+    return "This is catalog edit item !"
+
+# Allow Delete of Item
+@app.route('/catalog/<catalog_name>/<item_name>/delete/', methods=['GET', 'POST'])
+def deleteCategoryItem():
+    return "This is catalog delete item !"
+
+# JSON endpoint
+@app.route('/catalog.json/')
+def getCatalogJson():
+    return "This is catalog json return !"
+
+if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
+    app.debug = True
+    app.run(host='0.0.0.0', port=8000)
